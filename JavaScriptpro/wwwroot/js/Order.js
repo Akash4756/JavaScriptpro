639aa6152daa0a93c2d130b933221fc5df95cd06
Akash4756 <akashzebronic92@gmail.com>
2022-12-17 01:48:20 +0530

    Add project files.

---
+++ JavaScriptpro/wwwroot/js/Order.js
@@ -0,0 +1,24 @@
+﻿
+var menus = [
+    { "item": "Veg Noodles", "price": 150 },
+    { "item": "Paneer Noodles", "price": 200 },
+    { "item": "Veg Manchurian", "price": 200 },
+    { "item": "Paneer Chilly", "price": 250 },
+    { "item": "Veg Fried Rice", "price": 150 },
+    { "item": "Paneer Fried Rice", "price": 200 },
+    { "item": "Veg Rolls", "price": 100 },
+    { "item": "Paneer Rolls", "price": 130 }
+]
+
+$(document).ready(function () {
+    binMenuDDl()
+})
+
+function binMenuDDl() {
+    var op = "<option value='-1'>Select</option>"
+    menus.forEach(function (item) {
+        op += "<option value='" + item.price + "'>" + item.item + "</option>"
+    })
+    $("#ddlMenu").html(op)
+
+}
